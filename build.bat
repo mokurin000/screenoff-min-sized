9aab091aa561ef3ffda26ff5c4cc96d54254cac3
莯凛 <mokurin000@gmail.com>
2025-12-24 01:09:10 +0800

    enhance: 896 bytes now

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 nasm -fwin64 main.nasm -o main.obj
 
-link.exe /OPT:REF /NOLOGO ^
-    /FILEALIGN:512 /IGNORE:4108 /ALIGN:16 ^
+link.exe /OPT:REF /NOLOGO /FILEALIGN:512 ^
     /MERGE:.rdata=. /MERGE:.pdata=. ^
+    /IGNORE:4108 /ALIGN:16 ^
+    /IGNORE:4254 /MERGE:.text=. ^
     /SECTION:.,ER /ENTRY:_start /NODEFAULTLIB ^
     /subsystem:windows main.obj user32.lib
