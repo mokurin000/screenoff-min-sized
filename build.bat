84125318528edfd80e011ab53c1e02ae7b700e4b
莯凛 <mokurin000@gmail.com>
2025-12-24 01:01:18 +0800

    enhance: even smaller

--- build.bat
+++ build.bat
@@ -1,3 +1,8 @@
 @echo off
 nasm -fwin64 main.nasm -o main.obj
-link.exe /OPT:REF /FILEALIGN:512 /ENTRY:_start /subsystem:windows main.obj user32.lib
+
+link.exe /OPT:REF /NOLOGO ^
+    /FILEALIGN:512 /IGNORE:4108 /ALIGN:16 ^
+    /MERGE:.rdata=. /MERGE:.pdata=. ^
+    /SECTION:.,ER /ENTRY:_start /NODEFAULTLIB ^
+    /subsystem:windows main.obj user32.lib
